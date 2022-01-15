ea83d1e4cb62f5bb3cb39293e35b284dd32c467f
zhchok <khanas.serhii@gmail.com>
2022-01-15 11:24:03 +0200

    скрипт для модалки франшизы

--- src/js/modal.js
+++ src/js/modal.js
@@ -3,18 +3,12 @@
     openModalBtn: document.querySelector('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
-    openModalFranchBtn: document.querySelector('[data-modal-franchise-open]'),
-    closeModalFranchBtn: document.querySelector('[data-modal-franchise-close]'),
-    modalFranch: document.querySelector('[data-modal-franchise]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-  refs.openModalFranchBtn.addEventListener('click', toggleModal);
-  refs.closeModalFranchBtn.addEventListener('click', toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
-    refs.modalFranch.classList.toggle('is-hidden');
   }
 })();

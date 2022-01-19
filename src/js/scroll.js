77d7a934c60402a78d07435323d2b7d55d979450
ZeinHome <zein3288@gmail.com>
2022-01-19 13:34:45 +0200

    last fix

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,14 +1,13 @@
-$('.scrollto a').on('click', function() {
+// $('.scrollto a').on('click', function() {
 
-    let href = $(this).attr('href');
+//     const href = $(this).attr('href');
 
-    $('html, body').animate({
-        scrollTop: $(href).offset().top
-    }, {
-        duration: 370,   // по умолчанию «400» 
-        easing: "linear" // по умолчанию «swing» 
-    });
+//     $('html, body').animate({
+//         scrollTop: $(href).offset().top
+//     }, {
+//         duration: 370, // по умолчанию «400» 
+//         easing: "linear" // по умолчанию «swing» 
+//     });
 
-    return false;
-});
-
+//     return false;
+// });
